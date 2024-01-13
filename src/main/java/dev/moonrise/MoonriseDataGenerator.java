7651f17ebaf71aaef238361c17799f64fa610372
radmanplays <radmanfprn@gmail.com>
2024-01-13 19:00:01 +0330

    Initial commit

---
+++ src/main/java/dev/moonrise/MoonriseDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.moonrise;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoonriseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
