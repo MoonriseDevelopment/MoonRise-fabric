cb04490eea8a3afd3973455810f4ec89f09ba3a5
radmanplays <95340057+radmanplays@users.noreply.github.com>
2024-01-17 15:21:54 +0330

    add settings

--- src/main/java/dev/moonrise/module/settings/BooleanSetting.java
+++ src/main/java/dev/moonrise/module/settings/BooleanSetting.java
@@ -1,4 +1,21 @@
 package dev.moonrise.module.settings;
 
-public class BooleanSetting {
+public class BooleanSetting extends Setting{
+
+    private Boolean enabled;
+    public BooleanSetting(String name, boolean defaultvalue){
+        super(name);
+        this.enabled = defaultvalue;
+    }
+    public void toggle(){
+        this.enabled = !this.enabled;
+    }
+
+    public Boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
 }
--- src/main/java/dev/moonrise/module/settings/Setting.java
+++ src/main/java/dev/moonrise/module/settings/Setting.java
@@ -1,6 +1,17 @@
 package dev.moonrise.module.settings;
 
 public class Setting {
+    private String name;
+    private Boolean visible = true;
 
+    public Setting(String name){
+        this.name = name;
+    }
 
+    public Boolean isVisible() {
+        return visible;
+    }
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
 }
